fb47e079b483063362d041616c0d0914454fe197
Max <maximilian.paulsen@gmail.com>
2021-02-21 14:00:31 -0800

    Added minstret to perf driver

--- capsules/src/perf.rs
+++ capsules/src/perf.rs
@@ -21,16 +21,16 @@ impl Driver for Perf {
     ///
     /// - `0`: Driver check.
     /// - `1`: Get perf counter.
-<<<<<<< HEAD
-    fn command(&self, command_num: usize, data: usize, _: usize, _: AppId) -> ReturnCode {
-=======
+    /// - `2`: Get number of instructions executed
     fn command(&self, command_num: usize, _: usize, _: usize, _: AppId) -> ReturnCode {
->>>>>>> cd1bb8bc39036b5ed6c7ad0ab71dd5f5e2a1cf98
         match command_num {
             0 /* check if present */ => ReturnCode::SuccessWithValue { value: 1 },
 
             1 /* FIXME HACK This needs to be implemented in the HIL somehow */ =>
                 ReturnCode::SuccessWithValue { value: csr::CSR.mcycle.get() as usize },
+            
+            2 /* FIXME HACK This needs to be implemented in the HIL somehow */ =>
+                ReturnCode::SuccessWithValue { value: csr::CSR.minstret.get() as usize },
 
             _ => ReturnCode::ENOSUPPORT,
         }
